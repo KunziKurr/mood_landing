73b77cfb5e1ff73915ff73d369589966b7868614
Emmanuel Korir <korirm3@gmail.com>
2023-09-26 18:06:25 +0300

    Images Added

--- src/app/sects/_services.js
+++ src/app/sects/_services.js
@@ -10,15 +10,24 @@ const Services = () => {
 
     const images = [
         {
-          original: "https://picsum.photos/id/1018/1000/600/",
+          original: "../../assets/img/2Asset.svg",
         //   thumbnail: "https://picsum.photos/id/1018/250/150/",
         },
         {
-          original: "https://picsum.photos/id/1015/1000/600/",
+            original: "../../assets/img/Asset1.svg",
         //   thumbnail: "https://picsum.photos/id/1015/250/150/",
         },
         {
-          original: "https://picsum.photos/id/1019/1000/600/",
+            original: "../../assets/img/3Asset.svg",
+        //   thumbnail: "https://picsum.photos/id/1019/250/150/",
+        }, {
+            original: "../../assets/img/4Asset.svg",
+        //   thumbnail: "https://picsum.photos/id/1019/250/150/",
+        }, {
+            original: "../../assets/img/5Asset.svg",
+        //   thumbnail: "https://picsum.photos/id/1019/250/150/",
+        },{
+            original: "../../assets/img/6Asset.svg",
         //   thumbnail: "https://picsum.photos/id/1019/250/150/",
         },
       ]
