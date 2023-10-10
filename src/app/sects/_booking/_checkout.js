daaaff3dc4d8ba3b64e61f9b47f8abce21a6c103
KunziKurr <korirm3@gmail.com>
2023-10-10 08:30:57 +0300

    Checkout added

--- src/app/sects/_booking/_checkout.js
+++ src/app/sects/_booking/_checkout.js
@@ -11,9 +11,69 @@ const  Checkout = () => {
    
     return (
         <div className="checkout">
-        checkout
+                       <div className="people_wrapper">
+     <h3 className="people_wrapper_heading">
+     Checkout Details
+     </h3>  
+     <p className="people_wrapper_p">
+     Total for your Booking on <strong>17 August: <span>KES 12,300 </span></strong>
+     </p>
+
+<Details />
+     
+
+   
+      
+        <button className="button_next">
+            Continue
+        </button>
+     </div>
  
         </div>
     )
 }
 export default Checkout;
+
+
+const Details = () =>{
+    return(
+        <div className="form_details">
+        <div className="form_details_grid_cont">
+            <div className="form_details_container">
+                <label htmlFor="name" className="form_details_container_label">First Name</label>
+                <input type="text" className="form_details_container_input" />
+            </div>
+            <div className="form_details_container">
+                <label htmlFor="name" className="form_details_container_label">Last Name</label>
+                <input type="text" className="form_details_container_input" />
+            </div>
+        </div> 
+        <div className="form_details_grid_cont">
+            <div className="form_details_container">
+                <label htmlFor="name" className="form_details_container_label">Email Address</label>
+                <input type="text" className="form_details_container_input" />
+            </div>
+            <div className="form_details_container">
+                <label htmlFor="name" className="form_details_container_label">Mobile Number</label>
+                <input type="text" className="form_details_container_input" />
+            </div>
+        </div>
+        <div className="form_details_container">
+                <label htmlFor="name" className="form_details_container_label">Promo Code/Discount</label>
+                <input type="text" className="form_details_container_input" />
+            </div>
+
+
+            <div className="form_details_container">
+                <input type='checkbox' className="form_details_container_input" />
+                <label htmlFor="name" className="form_details_container_label">Promo Code/Discount</label>
+            </div>
+
+            <div className="form_details_container">
+                <input type="checkbox" className="form_details_container_input" />
+                <label htmlFor="name" className="form_details_container_label">Promo Code/Discount</label>
+            </div>
+
+        </div>
+    )
+}
--- src/app/sects/_booking/_extras.js
+++ src/app/sects/_booking/_extras.js
@@ -3,7 +3,7 @@ import svgService from '../../../svg_service'
 import { NavLink } from 'react-router-dom';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-
+import extrasImg from '../../../assets/img/extras.svg'
 
 const  Extras = () => {
     const [showMenu, setShowMenu] = useState(0)
@@ -11,7 +11,55 @@ const  Extras = () => {
    
     return (
         <div className="extras">
-        extras
+               <div className="people_wrapper">
+     <h3 className="people_wrapper_heading">
+       Enhance your visit
+     </h3>
+     <p className="people_wrapper_p">
+     You may like to add one of our signature Products
+     </p>
+
+     <div className="extras_wrapper">
+        <div className="extras_wrapper_extra">
+            <div className="extras_wrapper_extra_thumbnail">
+                 <img src={extrasImg} alt="" srcset="" />
+            </div>
+            <div className="extras_wrapper_extra_content">
+                <p className="extras_wrapper_extra_content_p">
+                    Signature Creme
+                </p>
+                <h2 className="extras_wrapper_extra_content_h2">
+                    KES 1,200.00
+                </h2>
+                <p className="extras_wrapper_extra_content_p">
+                    Signature Creme 
+                </p>
+            </div>
+        </div>
+
+        <div className="extras_wrapper_extra">
+            <div className="extras_wrapper_extra_thumbnail">
+             <img src={extrasImg} alt="" srcset="" />
+            </div>
+            <div className="extras_wrapper_extra_content">
+                <p className="extras_wrapper_extra_content_p">
+                    Signature Creme
+                </p>
+                <h2 className="extras_wrapper_extra_content_h2">
+                    KES 1,200.00
+                </h2>
+                <p className="extras_wrapper_extra_content_p">
+                    Signature Creme 
+                </p>
+            </div>
+        </div>
+
+     </div>
+      
+        <button className="button_next">
+            Continue
+        </button>
+     </div>
  
         </div>
     )
