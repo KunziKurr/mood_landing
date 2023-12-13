f6316173a0c5b532322042824e006a562a4f9f1b
KunziKurr <korirm3@gmail.com>
2023-12-13 22:34:13 +0300

    About & Contact

--- src/app/sects/_about_us.js
+++ src/app/sects/_about_us.js
@@ -1,9 +1,11 @@
 
 import svgService from '../../svg_service';
 import Header from '../commons/_header'
+import Footer from '../commons/_footer'
 
 import Asset3 from  '../../assets/img/3Asset.svg'
 import Meet from '../../assets/img/meet.png'
+import Team from '../../assets/img/team.svg'
 
 
 const About = () => {
@@ -31,9 +33,67 @@ const About = () => {
               </div>
                <div className="about_us_container_wrapper_sect_2">
                   <h1 className="heading_1 black">Why our clients choose us</h1>
+                  <p className="p_content black">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Maiores dolore eveniet ab, doloremque quidem sapiente, ratione deleniti eaque nobis in, 
+                    veniam enim magni? Itaque nihil unde consectetur, eligendi amet autem!
+                  </p>
               </div>
               </div>
             </section>
+            <div className="services_wrapper_transformation">
+                <div className="services_wrapper_transformation_wrapper">
+                    <h3 className="services_wrapper_transformation_wrapper_heading">
+                        OUR SERVICES
+                    </h3>
+                    <div className="services_wrapper_transformation_wrapper_services">
+                        <div className="service">
+                            <h4 className="service_heading">Shave</h4>
+                            <div className="service_line"></div>
+                            <button className="service_btn">View More</button>
+                        </div>
+
+                        <div className="service">
+                            <h4 className="service_heading"> Beard Treatment</h4>
+                            <div className="service_line"></div>
+                            <button className="service_btn">View More</button>
+                        </div>
+
+                        <div className="service">
+                            <h4 className="service_heading">Massages</h4>
+                            <div className="service_line"></div>
+                            <button className="service_btn">View More</button>
+                        </div>
+                    </div>
+                </div>
+              </div>
+              <div className="services_wrapper_transformation team">
+                <div className="services_wrapper_transformation_wrapper">
+                    <h3 className="services_wrapper_transformation_wrapper_heading">
+                        OUR TEAM
+                    </h3>
+                    <p className="p_content black">
+                    We employ the best manicurists and pedicurist in the industry.
+                     We also educate our own award-winning specialists through constant training. 
+                     Here are the most prominent talents.
+                    </p>
+                    <div className="services_wrapper_transformation_wrapper_services">
+                        <div className="service team">
+                           
+                        </div>
+
+                        <div className="service team">
+                           
+                        </div>
+
+                        <div className="service team">
+                           
+                        </div>
+                    </div>
+                </div>
+              </div>
+            <Footer />
+
         </div>
 
     </>
